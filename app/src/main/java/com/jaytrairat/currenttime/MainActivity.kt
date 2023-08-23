9dace90ea19530fc1144115f9926014cd1176007
jaytrairat <jaytrairat@outlook.com>
2023-08-23 16:20:06 +0700

    feat: land scape with edittext

--- app/src/main/java/com/jaytrairat/currenttime/MainActivity.kt
+++ app/src/main/java/com/jaytrairat/currenttime/MainActivity.kt
@@ -1,9 +1,18 @@
 package com.jaytrairat.currenttime
 
+import android.content.Context
+import android.content.res.Configuration
 import android.os.Bundle
 import android.util.Log
+import android.view.KeyEvent
+import android.view.MotionEvent
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.doOnTextChanged
 import com.jaytrairat.currenttime.databinding.ActivityMainBinding
 import java.text.SimpleDateFormat
 import java.util.*
@@ -13,13 +22,40 @@ import kotlin.concurrent.timerTask
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var caseNumberTxt: EditText
+    private fun hideKeyboardAndClearFocus(editText: EditText) {
+        val inputMethodManager =
+            getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(editText.windowToken, 0)
+        editText.clearFocus()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
+
+        caseNumberTxt = binding.caseNumberTxt
+        caseNumberTxt.setOnEditorActionListener { _, actionId, event ->
+            if (actionId == EditorInfo.IME_ACTION_GO) {
+                hideKeyboardAndClearFocus(caseNumberTxt)
+            }
+            true
+        }
+
         setContentView(binding.root)
 
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+
+        caseNumberTxt.setText("test")
+        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            caseNumberTxt.setText("test")
+        } else {
+            Toast.makeText(this@MainActivity, "Change view", Toast.LENGTH_LONG).show()
+        }
     }
 
     override fun onStart() {
@@ -36,13 +72,14 @@ class MainActivity : AppCompatActivity() {
             elmTimer.scheduleAtFixedRate(
                 timerTask()
                 {
-                    val patternDate = "วันที่ dd MMM YYYY"
-                    val simpleDateFormat = SimpleDateFormat(patternDate)
-                    val date = simpleDateFormat.format(Date())
 
                     val patternTime = "HH:mm:ss"
                     val simpleTimeFormat = SimpleDateFormat(patternTime)
                     val time = simpleTimeFormat.format(Date())
+
+                    val patternDate = "(dd MMM YYYY)"
+                    val simpleDateFormat = SimpleDateFormat(patternDate)
+                    val date = simpleDateFormat.format(Date())
 
                     this@MainActivity.runOnUiThread(Runnable {
                         var dateView = findViewById<TextView>(R.id.dateView)
